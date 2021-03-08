f8da6ad83c7890a1b94fabf7b30fb29a0dc01896
Matt <mateuszmierzejek187@gmail.com>
2021-03-08 20:50:32 +0100

    react extension snippet renders

--- src/index.js
+++ src/index.js
@@ -15,6 +15,37 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+/*
+Snippet	Renders
+
+imr	Import React
+imrc	Import React / Component
+imrs	Import React / useState
+imrse	Import React / useState useEffect
+impt	Import PropTypes
+impc	Import React / PureComponent
+cc	Class Component
+ccc	Class Component With Constructor
+cpc	Class Pure Component
+sfc	Stateless Function Component
+cdm	componentDidMount
+uef	useEffect Hook
+cwm	componentWillMount
+cwrp	componentWillReceiveProps
+gds	getDerivedStateFromProps
+scu	shouldComponentUpdate
+cwu	componentWillUpdate
+cdu	componentDidUpdate
+cwu	componentWillUpdate
+cdc	componentDidCatch
+gsbu	getSnapshotBeforeUpdate
+ss	setState
+ssf	Functional setState
+usf	Declare a new state variable using State Hook
+ren	render
+rprop	Render Prop
+hoc	Higher Order Component
+*/ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
