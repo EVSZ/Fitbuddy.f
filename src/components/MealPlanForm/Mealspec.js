0d88afad66982915dce11f94360d4e5c36fc3059
Matt <mateuszmierzejek187@gmail.com>
2021-03-08 13:21:41 +0100

    updated form with better looking controls
    added:
    calculator button
    calories/meal append section
    Updated the flow of css

--- src/components/MealPlanForm/Mealspec.js
+++ src/components/MealPlanForm/Mealspec.js
@@ -1,15 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, InputGroup } from 'react-bootstrap';
 
 function InputNmbSm(props) {
+    let button;
+
+    if(props.showBtn === "calcBtn") {
+        button = <Button className={props.showBtn} variant="outline-light">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
+  <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+  <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
+</svg>
+        </Button>;
+    } else {
+        button = '';
+    }
     return (
         <Form.Group>
             <div className="MealFormGrL">
                 <Form.Label> {props.visibleText} </Form.Label>      
             </div>
-            <div className="MealFormGrC">
-                <Form.Control type="number" step={props.step} placeholder={props.placeholder}></Form.Control>   
+            <div className="MealFormGrC">               
+                <InputGroup className="mb-3"> 
+                <Form.Control type="number" min={props.min} step={props.step} value={props.placeholder} /> 
+                    <InputGroup.Append>
+                    <InputGroup.Text className={props.lengthOfPh}>{props.input}</InputGroup.Text>
+                    {button}
+                    </InputGroup.Append>
+                </InputGroup>
             </div>
         </Form.Group>
     );
@@ -39,10 +57,10 @@ function Mealspec() {
                 <CheckDietType image="Images/dietKeto.png" dietType="Keto" btnVar="outline-primary"></CheckDietType>
             </div>
             <div className="MealFormEl">
-                <InputNmbSm visibleText="I want to eat" step="100"placeholder="calories"></InputNmbSm>
+                <InputNmbSm visibleText="I want to eat" showBtn="calcBtn" input="calories" min="100" step="100" placeholder="2500"></InputNmbSm>
             </div>                
             <div className="MealFormEl">
-                <InputNmbSm visibleText="In" placeholder="meals" step="1" type="withCalc"></InputNmbSm>
+                <InputNmbSm visibleText="In" lengthOfPh="longAppend" input="meals" showBtn="hidden"  min="1" step="1" placeholder="4"></InputNmbSm>
             </div>
             <div className="MealFormEl">        
                 {/*<Button variant="outline-info" type="submit" size="sm">Unsure?</Button> */}
